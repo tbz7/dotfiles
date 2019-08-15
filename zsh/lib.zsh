e3999815af27bb38ebefa843ae80d8363fd94bef
tbz7 <46201109+tbz7@users.noreply.github.com>
2019-08-15 17:55:13 -0600

    zsh 5.7

--- zsh/lib.zsh
+++ zsh/lib.zsh
@@ -56,7 +56,7 @@ __plugins=()
 export COLORTERM=truecolor
 unset NO_CURSOR_SHAPES
 if [[ $TERM_PROGRAM == Apple_Terminal && $TERM == xterm-256color ]] TERM=nsterm
-if [[ $TERM == nsterm* ]] unset COLORTERM
+if [[ $TERM == nsterm* ]] unset COLORTERM && zmodload zsh/nearcolor
 if [[ $TERM == linux* ]] export NO_CURSOR_SHAPES=true NO_CUSTOM_FONT=true
 
 
--- zsh/modules/prompt.zsh
+++ zsh/modules/prompt.zsh
@@ -1,21 +1,21 @@
-psvar=('' '' '' '5;'{233,233,233,110,107,236,110,107,188,235,235,234,110,107,188,234,234,233,233,167,244})
-local names=(vcs vicmd ia {a,b,c}{f,b}g{,2,i} efg ebg comment_fg) n=({1..$#psvar}) e=$'\e'
+psvar=('' '' '' 233 233 233 110 107 236 110 107 188 235 235 234 110 107 188 234 234 233 233 167 244)
+local names=(vcs vicmd ia {a,b,c}{f,b}g{,2,i} efg ebg comment_fg) n=({1..$#psvar})
 local -A i=(${names:^n})
 
 for x in a b c; do
-  local FF$x="%{${e}[38;%($i[ia]V.%$i[${x}fgi]v.%($i[vicmd]V.%$i[${x}fg2]v.%$i[${x}fg]v))m%}"
-  local KK$x="%{${e}[48;%($i[ia]V.%$i[${x}bgi]v.%($i[vicmd]V.%$i[${x}bg2]v.%$i[${x}bg]v))m%}"
-  local FK$x="%{${e}[38;%($i[ia]V.%$i[${x}bgi]v.%($i[vicmd]V.%$i[${x}bg2]v.%$i[${x}bg]v))m%}"
+  local FF$x="%F{%($i[ia]V.%$i[${x}fgi]v.%($i[vicmd]V.%$i[${x}fg2]v.%$i[${x}fg]v))}"
+  local KK$x="%K{%($i[ia]V.%$i[${x}bgi]v.%($i[vicmd]V.%$i[${x}bg2]v.%$i[${x}bg]v))}"
+  local FK$x="%F{%($i[ia]V.%$i[${x}bgi]v.%($i[vicmd]V.%$i[${x}bg2]v.%$i[${x}bg]v))}"
 done
-local FFe="%{${e}[38;%($i[ia]V.%$i[cfgi]v.%$i[efg]v)m%}"
-local KKe="%{${e}[48;%($i[ia]V.%$i[cbgi]v.%$i[ebg]v)m%}"
-local FKe="%{${e}[38;%($i[ia]V.%$i[cbgi]v.%$i[ebg]v)m%}"
+local FFe="%F{%($i[ia]V.%$i[cfgi]v.%$i[efg]v)}"
+local KKe="%K{%($i[ia]V.%$i[cbgi]v.%$i[ebg]v)}"
+local FKe="%F{%($i[ia]V.%$i[cbgi]v.%$i[ebg]v)}"
 
 if [[ $NO_CUSTOM_FONT != true ]]; then
   PROMPT="$FFa$KKa %m $FKa$KKb$FFb %33<⋯ <%~ %k$FKb%f "
   RPROMPT="%(?..$FKe$FFe$KKe ✗ %($i[vcs]V.$FKc.))%($i[vcs]V.%(?.$FKc.)$FFc$KKc %$i[vcs]v .)%f%k"
-  PROMPT2="%{${e}[38;%$i[comment_fg]vm%}↳  %f"
-  RPROMPT2="%{${e}[38;%$i[comment_fg]vm%} %1^%f"
+  PROMPT2="%F{%$i[comment_fg]v}↳  %f"
+  RPROMPT2="%F{%$i[comment_fg]v} %1^%f"
 else
   PROMPT="%($i[vicmd]V.%2F%m%f:%3F.%4F%m%f:%2F)%33<...<%~%f %(!.#.$) "
   RPROMPT="%(?..%S%1F X %f%s)%($i[vcs]V. %$i[vcs]v .)"
@@ -24,15 +24,11 @@ fi
 
 #-------------------------------------------------------------------------------
 
-if [[ $NO_CUSTOM_FONT != true && $COLORTERM == truecolor ]]; then
-  hook theme prompt "
-    local c k v; local -A i=(${(kv)i})
-    zstyle -a ':theme' colors c
-    for k v in \$c; do
-      if (( \$i[\$k] )) psvar[\$i[\$k]]=\"2;\$((0x\${v:0:2}));\$((0x\${v:2:2}));\$((0x\${v:4}))\"
-    done
-  "
-fi
+hook theme prompt "
+  local c k v; local -A i=(${(kv)i})
+  zstyle -a ':theme' colors c
+  for k v (\$c) (( \$i[\$k] )) && psvar[\$i[\$k]]=\"#\$v\"
+"
 
 hook precmd prompt-vicmd "psvar[$i[vicmd]]="
 hook zle_keymap prompt-vicmd "psvar[$i[vicmd]]=\${(M)KEYMAP:#vicmd}; zle reset-prompt"
